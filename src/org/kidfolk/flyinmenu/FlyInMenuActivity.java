fbb8444a9b0fdc9442ff22ba71f22a89e419ca11
kidfolk <kidfolk@gmail.com>
2012-06-08 18:22:30 +0800

    初步完成手势滑动

--- src/org/kidfolk/flyinmenu/FlyInMenuActivity.java
+++ src/org/kidfolk/flyinmenu/FlyInMenuActivity.java
@@ -60,56 +60,6 @@ public class FlyInMenuActivity extends Activity {
 		return super.onTouchEvent(event);
 	}
 
-	class ImageAdapter extends BaseAdapter {
-
-		private Context mContext;
-
-		// references to our images
-		private Integer[] mThumbIds = { R.drawable.sample_2,
-				R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5,
-				R.drawable.sample_6, R.drawable.sample_7, R.drawable.sample_0,
-				R.drawable.sample_1, R.drawable.sample_2, R.drawable.sample_3,
-				R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_6,
-				R.drawable.sample_7, R.drawable.sample_0, R.drawable.sample_1,
-				R.drawable.sample_2, R.drawable.sample_3, R.drawable.sample_4,
-				R.drawable.sample_5, R.drawable.sample_6, R.drawable.sample_7 };
-
-		public ImageAdapter(Context context) {
-			this.mContext = context;
-		}
-
-		@Override
-		public int getCount() {
-			return mThumbIds.length;
-		}
-
-		@Override
-		public Object getItem(int position) {
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return 0;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ImageView imageView;
-			if (convertView == null) {
-				imageView = new ImageView(mContext);
-				imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				imageView.setPadding(8, 8, 8, 8);
-			} else {
-				imageView = (ImageView) convertView;
-			}
-			imageView.setImageResource(mThumbIds[position]);
-			return imageView;
-		}
-
-	}
-
 	private class NavigationItemAdapter extends BaseAdapter {
 
 		NavigationItem[] items = { new NavigationItem("社区", "精选.活动.人物"),
