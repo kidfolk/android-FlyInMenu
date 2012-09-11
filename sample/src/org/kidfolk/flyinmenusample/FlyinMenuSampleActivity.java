b600029803951c12120c9073052f37912c22a9b2
kidfolk <kidfolk@gmail.com>
2012-09-11 15:30:46 +0800

    modify sample project

--- sample/src/org/kidfolk/flyinmenusample/FlyinMenuSampleActivity.java
+++ sample/src/org/kidfolk/flyinmenusample/FlyinMenuSampleActivity.java
@@ -5,10 +5,7 @@ import org.kidfolk.flyinmenu.RootView;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -24,51 +21,34 @@ public class FlyinMenuSampleActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		getSupportFragmentManager().beginTransaction().add(R.id.host, new HostFragment()).commit();
-		
+
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.host, new HostFragment()).commit();
+
 		ListView listView = (ListView) findViewById(R.id.list);
 		listView.setAdapter(new NavigationItemAdapter(this
 				.getApplicationContext()));
 
 		mRootView = (RootView) findViewById(R.id.root);
 	}
-	
-	RootView getRootView(){
+
+	RootView getRootView() {
 		return mRootView;
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		Log.d(TAG, "keyCode:" + keyCode);
-		return super.onKeyDown(keyCode, event);
-	}
-
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		// TODO Auto-generated method stub
-		return super.dispatchTouchEvent(ev);
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// TODO Auto-generated method stub
-		return super.onTouchEvent(event);
 	}
 
 	private class NavigationItemAdapter extends BaseAdapter {
 
-		NavigationItem[] items = { new NavigationItem("社区", "精选.活动.人物"),
-				new NavigationItem("社区", "社区小组"),
-				new NavigationItem("小明", "新鲜事"),
-				new NavigationItem("小明", "我的箱子"),
-				new NavigationItem("小明", "我的朋友"),
-				new NavigationItem("小明", "消息"), new NavigationItem("小明", "主页"),
-				new NavigationItem("小明", "收藏"),
-				new NavigationItem("我的小组", "hello"),
-				new NavigationItem("更多操作", "搜索"),
-				new NavigationItem("更多操作", "设置"),
-				new NavigationItem("更多操作", "休眠") };
+		NavigationItem[] items = { new NavigationItem("section1", "title1"),
+				new NavigationItem("section1", "title2"),
+				new NavigationItem("section2", "title1"),
+				new NavigationItem("section2", "title2"),
+				new NavigationItem("section2", "title3"),
+				new NavigationItem("section2", "title4"),
+				new NavigationItem("section2", "title5"),
+				new NavigationItem("setcion3", "title1"),
+				new NavigationItem("section4", "title1"),
+				new NavigationItem("section4", "title2"),
+				new NavigationItem("section4", "title3") };
 
 		class NavigationItem {
 			String group;
